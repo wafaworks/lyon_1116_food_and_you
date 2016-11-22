6c862e8a9dfc24d450c3722d83ae6229de968ad1
ngdo-pro <ngdo27@gmail.com>
2016-11-22 14:21:26 +0100

    finished console command

--- src/AppBundle/Command/SlugifyExistantDataCommand.php
+++ src/AppBundle/Command/SlugifyExistantDataCommand.php
@@ -9,11 +9,14 @@
 namespace AppBundle\Command;
 
 
-use Symfony\Component\Console\Command\Command;
+use AppBundle\Entity\Event;
+use AppBundle\Entity\Restaurant;
+use Cocur\Slugify\Slugify;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class SlugifyExistantDataCommand extends Command
+class SlugifyExistantDataCommand extends ContainerAwareCommand
 {
     protected function configure()
     {
@@ -25,6 +28,23 @@ class SlugifyExistantDataCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // ...
+
+        $em = $this->getContainer()->get('doctrine')->getManager();
+        $restaurants = $this->getContainer()->get('app.repository.restaurant')->findAll();
+        $events = $this->getContainer()->get('app.repository.event')->findAll();
+        /** @var Restaurant $restaurant */
+        foreach ($restaurants as $restaurant){
+            $slugify = new Slugify();
+            $restaurant->setSlug($slugify->slugify($restaurant->getName()));
+            $em->persist($restaurant);
+        }
+        /** @var Event $event */
+        foreach ($events as $event){
+            $slugify = new Slugify();
+            $toBeSlugified = $event->getRestaurant()->getName() . ' ' . $event->getStartDate()->format('d-m-Y');
+            $event->setSlug($slugify->slugify($toBeSlugified));
+            $em->persist($event);
+        }
+        $em->flush();
     }
 }
